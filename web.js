b430022dae600c5f51eb0743c478fa1bbfe9dd2c
Jay Falck <coursera@unicorncomputing.com>
2013-07-07 11:36:06 -0700

    Yet another attempt

--- web.js
+++ web.js
@@ -1,12 +1,10 @@
 var express = require('express');
-var fs = require('fs');
 var app = express.createServer(express.logger());
-var buf = new Buffer (256);
-var str2 = buf.toString('utc 8', fs.readFileSync('./index.html'));
-var str  = 'Hello Jay';
 
 app.get('/', function(request, response) {
-  response.send(str2);
+  var fs = require('fs');
+  var buf = new Buffer (256);
+  response.send(buf.toString('utc 8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
